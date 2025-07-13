9dbb15c9766b6b7d9b10801788e05756e1092051
Max Andreev <maxick20@gmail.com>
2025-07-13 17:29:01 +0500

    Install vitest

---
+++ test/collection.test.ts
@@ -0,0 +1,7 @@
+import { test, expect, describe, beforeAll, afterAll } from "vitest";
+
+describe("NFTs pallet Collections", () => {
+  test("create", async () => {
+    expect(true).toBe(true);
+  });
+});
