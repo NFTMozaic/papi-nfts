8179e9c244667a8fed180c11a4251b09fc6b934b
Max Andreev <maxick20@gmail.com>
2025-08-07 18:53:12 +0500

    Query account collections and update trading swap direction to use Enum

--- tests/collection.test.ts
+++ tests/collection.test.ts
@@ -5,10 +5,25 @@ import { COLLECTION_DEPOSIT } from "./utils/constants";
 import { Enum } from "polkadot-api";
 
 describe("NFTs pallet Collections", () => {
-  test("create", async ({ api, signers }) => {
+  test("account can create collection and query owned collections", async ({ api, signers }) => {
     const {alice, bob} = signers;
 
     const createCollectionTx = await api.tx.Nfts.create({
+      admin: MultiAddress.Id(bob.address),
+      config: {
+        max_supply: 1000,
+        mint_settings: {
+          default_item_settings: 0n,
+          mint_type: Enum("Issuer"),
+          price: 1n * 10n ** 10n, // 1 DOT
+          start_block: undefined,
+          end_block: undefined,
+        },
+        settings: 0n,
+      },
+    }).signAndSubmit(alice);
+
+    const createCollectionT2x = await api.tx.Nfts.create({
       admin: MultiAddress.Id(bob.address),
       config: {
         max_supply: 1000,
@@ -30,5 +45,10 @@ describe("NFTs pallet Collections", () => {
     const collectionInfo = await api.query.Nfts.Collection.getValue(collectionId);
     expect(collectionInfo?.owner).toBe(alice.address);
     expect(collectionInfo?.owner_deposit).toBe(COLLECTION_DEPOSIT);
+
+    // Can query account collections
+    const ownedCollections = await api.query.Nfts.CollectionAccount.getEntries(alice.address);
+    const collectionIds = ownedCollections.map(c => c.keyArgs[1]);
+    expect(collectionIds).contain(collectionId)
   });
 });
